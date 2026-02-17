4640dc0ae2ff6902cf30c293ff61309a60100f48
AljazKnific <knific.alja@gmail.com>
2026-02-17 23:46:46 +0100

    AOC-2015 DAY9

--- Aoc2015/Day_9/Day9.py
+++ Aoc2015/Day_9/Day9.py
@@ -1,80 +1,91 @@
 f = open("Input.txt", "r")
 
-class Path:
-    def __init__(self,x,y) -> None:
-        self.name = x
-        self.value = y
 
-class City: 
-    def __init__(self, name) -> None:
-        self.name = name
-        self.optimal = 10000000
-        self.paths = []
-        
-    def addPath(self, name, value) -> None:
-        if value < self.optimal:
-            self.optimal = value
-            self.paths.insert(0, Path(name, value))
-        else:
-            self.paths.append(Path(name, value))
-        
-def checkCity(name, graph):
-        
-        for x in graph:
-            if x.name == name:
-                return x, True
-            
-        return City(name), False
-    
-def graphTraversal(graph, name, visited):
-    if len(graph) == len(visited):
-        return 0
-    
-    if name == '':
-        name = list(graph)[0].name
-        visited.add(name)
-    
-    for x in graph:
-        if x.name == name:
-            for y in x.paths:
-                if y.name not in visited:
-                    visited.add(y.name)
-                    return y.value + graphTraversal(graph, y.name, visited)
-                    break
-             
-    return 0    
-    
-def first():
-    
-    graph = set()
-    
-    while 1:
-        
-        line = f.readline()
-        
-        if not line:
-            break
-        
-        data = line.split(' ')
-        data[len(data) - 1] = data[len(data) - 1].replace('\n', '')
+def nearest_neighbor_tsp(distances):
+    bestDistance = float('inf')
 
-        distance = int(data[4])
-        cityA, cityACheck = checkCity(data[0], graph)
-        cityB, cityBCheck = checkCity(data[2], graph)
-        
-        cityA.addPath(data[2],distance)
-        cityB.addPath(data[0], distance)
-        if not cityACheck:
-            graph.add(cityA)
-        if not cityBCheck:
-            graph.add(cityB)
-    
-    print(graphTraversal(graph, '', set()))    
-    for x in graph:
-        print(x.name)
-        for y in x.paths:
-            print("value: " + str(y.value) + " -> " + y.name)
-        
-    return 0
+    for start in range(len(distances)):
 
-first()
+        n = len(distances)
+        visited = [False] * n
+        route = [start]
+        visited[start] = True
+        total_distance = 0
+
+        for _ in range(1, n):
+            last = route[-1]
+            nearest = None
+            min_dist = float('inf')
+            for i in range(n):
+                if not visited[i] and distances[last][i] < min_dist:
+                    min_dist = distances[last][i]
+                    nearest = i
+            route.append(nearest)
+            visited[nearest] = True
+            total_distance += min_dist
+
+        if total_distance < bestDistance:
+            bestDistance = total_distance
+
+    return route, bestDistance
+
+def most_farthest_neighbor_tsp(distances):
+    bestDistance = 0
+
+    for start in range(len(distances)):
+
+        n = len(distances)
+        visited = [False] * n
+        route = [start]
+        visited[start] = True
+        total_distance = 0
+
+        for _ in range(1, n):
+            last = route[-1]
+            farthest = None
+            max_dist = float('-inf')
+            for i in range(n):
+                if not visited[i] and distances[last][i] > max_dist:
+                    max_dist = distances[last][i]
+                    farthest = i
+            route.append(farthest)
+            visited[farthest] = True
+            total_distance += max_dist
+
+        if total_distance > bestDistance:
+            bestDistance = total_distance
+
+    return route, bestDistance
+
+
+size = 8
+matrix = [[0 for i in range(size)] for j in range(size)]
+
+country = 0
+currCountry  = ""
+index = 1
+
+for line in f:
+    line = line.strip().split()
+    distance = int(line[4])
+
+    if currCountry == "":
+        currCountry = line[0]
+    elif currCountry != line[0]:
+        country += 1
+        currCountry = line[0]
+        index = country + 1
+
+    matrix[country][index] = distance
+    matrix[index][country] = distance
+    index += 1
+
+print(matrix)
+
+route, total_distance = nearest_neighbor_tsp(matrix)
+route2, total_distance2 = most_farthest_neighbor_tsp(matrix)
+print("Route:", route)
+print("Total Distance:", total_distance)
+
+print("Route:", route2)
+print("Total Distance:", total_distance2)
